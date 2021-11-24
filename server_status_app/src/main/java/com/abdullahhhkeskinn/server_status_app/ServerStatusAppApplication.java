af2ab6d52730281f64fad1289a973f177263e055
abdullahhhkeskinn <abdullahhhkeskinn@gmail.com>
2021-11-24 12:14:27 +0300

    First commit

---
+++ server_status_app/src/main/java/com/abdullahhhkeskinn/server_status_app/ServerStatusAppApplication.java
@@ -0,0 +1,13 @@
+package com.abdullahhhkeskinn.server_status_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStatusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStatusAppApplication.class, args);
+	}
+
+}
---
+++ server_status_app/src/test/java/com/abdullahhhkeskinn/server_status_app/ServerStatusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullahhhkeskinn.server_status_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStatusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
